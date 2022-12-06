c8e01191bd6a95b86b85bac734d647eabed2ce34
BlockAyusTred <109907283+BlockAyusTred@users.noreply.github.com>
2022-12-06 12:13:58 +0530

    Update SendWithdrawMoney.sol
    added some more functionality

--- [Project] Smart Money/SendWithdrawMoney.sol
+++ [Project] Smart Money/SendWithdrawMoney.sol
@@ -4,23 +4,95 @@ pragma solidity 0.8.14;
 
 contract SendWithdrawMoney {
 
-    uint public balanceRecieved;
+    address payable public owner;
+    uint public balance;
+    mapping(address => uint) public allowance;
+    mapping(address => bool) public isAlloweded;
+    mapping(address => bool) public guardians;
 
-    function deposit() public payable {
-        balanceRecieved+= msg.value;
+    address payable nextOwner;
+    uint guardiansResetCount;
+    uint public constant confirmationFromGuardiansForReset = 3;
+    mapping(address => mapping(address=>bool)) public nextOwnerGuardianVotedBool;
+
+    constructor() {
+        owner = payable(msg.sender);
     }
 
-    function getContractBalance() public view returns(uint) {
+    receive() external payable {
+        deposit();
+    }
+
+    function deposit() public payable {
+        balance += msg.value;
+    }
+
+    function getBalance() public view returns(uint) {
         return address(this).balance;
     }
 
-    function withdrawAll() public {
-        address payable to = payable(msg.sender);
-        to.transfer(getContractBalance());
+    function proposingNewOwner(address payable _newOwner) public {
+        require(guardians[msg.sender], " you are not guardian of this wallet, aborting");
+        require(nextOwnerGuardianVotedBool[_newOwner][msg.sender] == false , "you already voted, aborting");
+        if(_newOwner != nextOwner) {
+            nextOwner = _newOwner;
+            guardiansResetCount =0;
+        }
+
+        guardiansResetCount++;
+        nextOwnerGuardianVotedBool[_newOwner][msg.sender] = true;
+
+        if(guardiansResetCount >= confirmationFromGuardiansForReset) {
+            owner = nextOwner;
+            nextOwner = payable(address(0));
+        }
     }
 
-    function withdrawToAddress(address payable to) public {
-        to.transfer(getContractBalance());
+    function setGuardian(address _guardian, bool _isGuardian) public onlyOwner {
+        guardians[_guardian] = _isGuardian;
     }
 
+    function giveAllowance(address _to, uint amount) public onlyOwner sufficientBalance(amount) {
+        allowance[_to] += amount;
+        if(amount >0) {
+            isAlloweded[_to] = true;
+        }else {
+            isAlloweded[_to] = false;
+        }
+    }
+
+    function withdraw(uint amount) public onlyOwner sufficientBalance(amount) {
+        owner.transfer(amount);
+    }
+
+    function withdrawAll() public onlyOwner {
+        owner.transfer(getBalance());
+    }
+
+    function transfer(address payable _to, uint _amount) public onlyOwner sufficientBalance(_amount) {
+        _to.transfer(_amount);
+    }
+
+    function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory) {
+        if(msg.sender != owner) {
+            require(isAlloweded[msg.sender], "you are not allowded to send from this smart contract, Aborting");
+            require(allowance[msg.sender] >= _amount, "you are trying to send more than you are allowed to, Aborting");
+
+            allowance[msg.sender] -= _amount;
+        }
+
+        (bool success, bytes memory returnData) = _to.call{value: _amount}(_payload);
+        require(success, "Aborting, call was not successfull");
+        return returnData;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "you are not the owner");
+        _;
+    }
+
+    modifier sufficientBalance(uint amount) {
+        require(amount <= getBalance(), "Insufficeint balance");
+        _;
+    }
 }
